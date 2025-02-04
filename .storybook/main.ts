c73b435b68680d23bb7e4565944d3da0def0bece
gtn-74 <gtn.r74@gmail.com>
2025-02-04 23:39:54 +0900

    lib: install storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,24 +1,17 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
-
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-onboarding",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
     "@storybook/addon-interactions",
+    "@storybook/addon-outline/manager",
   ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
 };
-export default config;
 
-//! getAbsolutePath 必要ない説
-// 気が向いたら調べる程度にしとく
+export default config;
