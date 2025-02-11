312a188fa9f2b2647182c9aa1732f661ca176bf5
gtn-74 <gtn.r74@gmail.com>
2025-02-11 17:53:51 +0900

    lib: install storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,14 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx|mdx)"],
   addons: [
+    '@storybook/addon-actions',
     "@storybook/addon-onboarding",
     "@storybook/addon-docs",
     "@storybook/addon-interactions",
     "@storybook/addon-outline/manager",
+    '@storybook/addon-interactions',
   ],
   framework: {
     name: "@storybook/react-vite",
